bba802623e342951b451325577db189005659f74
clementleplusbo <clement1.ruat@epitech.eu>
2022-11-09 15:08:53 +0900

    feat: add player scene and map nul

---
+++ Game/Script/Camera.gd
@@ -0,0 +1,9 @@
+extends Camera2D
+
+onready var player = get_node("../Player")
+
+func _process(delta):
+	position = player.global_position
+	var x = floor(position.x)
+	var y = floor(position.y)
+	global_position = Vector2(x, y)
