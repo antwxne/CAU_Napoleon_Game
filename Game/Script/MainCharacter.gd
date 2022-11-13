6d681e49c3543056b1f456361e83e2defc9e8adb
theo <theo.campos@epitech.eu>
2022-11-14 01:16:13 +0900

    Feat: particule on the movement of the character and a menu (really ugly)

--- Game/Script/MainCharacter.gd
+++ Game/Script/MainCharacter.gd
@@ -4,39 +4,55 @@ export (int) var speed = 200
 
 onready var _animated_sprite = $AnimatedSprite
 
-var velocity = Vector2()
+export var velocity = Vector2()
 
 func get_input():
 	velocity = Vector2()
-	
-	
+
 	if Input.is_action_pressed("Down"):
+		$ParticuleMove.emitting = true;
+		$ParticuleMove.direction.x = 0;
+		$ParticuleMove.direction.y = -1;
 		velocity.y += 1
 		_animated_sprite.play("down_direction")
 		if Input.is_action_pressed("Right"):
-			 velocity.x += 1	
-			 _animated_sprite.play("down_direction")
+			$ParticuleMove.direction.x = -1;
+			velocity.x += 1
+			_animated_sprite.play("down_direction")
 		if Input.is_action_pressed("Left"):
+			$ParticuleMove.direction.x = 1;
 			velocity.x -= 1
 			_animated_sprite.play("down_direction")
 	elif Input.is_action_pressed("Up"):
+		$ParticuleMove.emitting = true;
+		$ParticuleMove.direction.x = 0;
+		$ParticuleMove.direction.y = 1;
 		velocity.y -= 1
 		_animated_sprite.play("up_direction")
 		if Input.is_action_pressed("Right"):
-			velocity.x += 1	
+			$ParticuleMove.direction.x = -1;
+			velocity.x += 1
 			_animated_sprite.play("up_direction")
 		if Input.is_action_pressed("Left"):
+			$ParticuleMove.direction.x = 1;
 			velocity.x -= 1
 			_animated_sprite.play("up_direction")
 	elif Input.is_action_pressed("Left"):
+		$ParticuleMove.emitting = true;
+		$ParticuleMove.direction.x = 1;
+		$ParticuleMove.direction.y = 0;
 		velocity.x -= 1
 		_animated_sprite.play("left_direction")
 	elif Input.is_action_pressed("Right"):
-		velocity.x += 1	
+		$ParticuleMove.emitting = true;
+		$ParticuleMove.direction.x = -1;
+		$ParticuleMove.direction.y = 0;
+		velocity.x += 1
 		_animated_sprite.play("right_direction")
 	else:
+		$ParticuleMove.emitting = false;
 		_animated_sprite.play("idle")
-		
+
 	velocity = velocity.normalized() * speed
 
 
---
+++ Game/Script/Menu.gd
@@ -0,0 +1,20 @@
+extends Node
+
+
+func _ready():
+	$VBoxContainer/Play.grab_focus();
+
+func _on_Play_pressed():
+	get_tree().change_scene("res://Main.tscn");
+
+
+func _on_Player_Stats_pressed():
+	pass # Replace with function body.
+
+
+func _on_Settings_pressed():
+	pass # Replace with function body.
+
+
+func _on_Exit_pressed():
+	get_tree().quit();
