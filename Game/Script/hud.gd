a3631df4b54d74b4589d20e53f561d1bf510ffc9
theo <theo.campos@epitech.eu>
2022-12-05 18:49:15 +0900

    Feat: health move when the player take dmg

--- Game/Script/hud.gd
+++ Game/Script/hud.gd
@@ -1,13 +1,16 @@
 extends Control
 
-var gold = 10;
+var gold = Save.gameData.player.gold;
 var time = 0;
+var health = 100 + (Save.gameData.player.health * 10);
+var baseHealth = 100 + Save.gameData.player.health * 10;
 
 func _ready():
-	gold = Save.gameData.player.gold;
 	update();
 	
 func update():
+	print(Save.gameData.player.health);
+	print(health);
 	$CanvasLayer/VBoxContainer/Label2.text = "Gold: " + str(gold);
 
 func _process(delta):
@@ -16,3 +19,9 @@ func _process(delta):
 	var sec = fmod(time, 60);
 	var mins = fmod(time, 60 * 60) / 60;
 	$CanvasLayer/VBoxContainer/Label.text = "%02d : %02d : %03d" % [mins, sec, mils];
+
+
+func _on_hurtbox_hurt(damage, angle, knockback):
+	health -= damage;
+	$CanvasLayer/VBoxContainer/health.value = (health * 100 / baseHealth);
+
